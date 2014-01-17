bfa0cf0954d27846bdb831182d8845265c0205bd
Jeremy Prevost <jerrocks@gmail.com>
2014-01-17 11:57:51 -0600

    Attempted Bugfix for Fedora / CentOS bug

--- lib/exlibris/primo/facet.rb
+++ lib/exlibris/primo/facet.rb
@@ -31,7 +31,7 @@ module Exlibris
       alias :count :size
 
       def facet_values
-        @facet_values ||= xml.root.search("//FACET_VALUES").collect do |facet_value|
+        @facet_values ||= xml_without_namespaces.root.search("//FACET_VALUES").collect do |facet_value|
           FacetValue.new(:raw_xml => facet_value.to_xml, :facet => self)
         end
       end
